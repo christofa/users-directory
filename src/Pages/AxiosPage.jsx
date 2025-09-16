384aa3e3f86de6e8d7caf22027a156b8c81644f1
Christopher <christofaokoro@gmail.com>
2025-09-16 15:41:20 +0100

    Page/dependencies setups

---
+++ src/Pages/AxiosPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AxiosPage() {
+  return (
+    <div>
+      <h1>AxiosPage</h1>
+    </div>
+  )
+}
+
+export default AxiosPage
---
+++ src/Pages/FetchPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FetchPage() {
+  return (
+    <div>
+        <h1>Fetch page</h1>
+      
+    </div>
+  )
+}
+
+export default FetchPage
